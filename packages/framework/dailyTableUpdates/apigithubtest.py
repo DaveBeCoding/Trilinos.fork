fd728b1a38022b850c7d1d4b91c40030e4c3f6f3
davebecoding <david00100johnson@gmail.com>
2023-02-24 11:21:26 -0800

    testing various automated mehtods to get the numerical values for the table script with no user input

---
+++ packages/framework/dailyTableUpdates/apigithubtest.py
@@ -0,0 +1,40 @@
+# import requests
+# import json
+
+# url = "https://api.github.com/repos/trilinos/Trilinos/pulls?q=is%3Apr+merged%3A2023-02-23T12%3A00%3A00-07%3A00..2023-02-24T12%3A00%3A00-07%3A00+base%3Adevelop"
+
+# response = requests.get(url)
+# data = json.loads(response.text)
+# # print(data)
+# for pr in data:
+#     if pr["state"] == "closed":
+#         print(f"#{pr['number']}: {pr['title']}")
+
+
+
+import requests
+import json
+
+url = "https://api.github.com/repos/trilinos/Trilinos/pulls?q=is%3Apr+merged%3A2023-02-22T12%3A00%3A00-07%3A00..2023-02-23T12%3A00%3A00-07%3A00+base%3Adevelop"
+
+# Set the API key
+api_key = ""
+
+# Set the headers with the API key
+headers = {
+    "Authorization": f"Bearer {api_key}"
+}
+
+# Make the request with the headers
+response = requests.get(url, headers=headers)
+
+print(f"Response status code: {response.status_code}")
+
+data = json.loads(response.text)
+
+print(json.dumps(data, indent=4))
+
+for pr in data:
+    print(f"PR #{pr['number']} state: {pr['state']}")
+    if pr["state"] == "closed":
+        print(f"#{pr['number']}: {pr['title']}")
